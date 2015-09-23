b00a85760b21b646c75ac2300a00e27e8963d9fe
Z <zhqhku@gmail.com>
2015-09-23 10:37:12 +0800

    DataGen: done formatting the data set directory

--- LogReg/DataGen.py
+++ LogReg/DataGen.py
@@ -4,27 +4,54 @@ this script is to generate training data set for LogReg benchmark
 from util.EmbedScript import run_script, ScriptException
 
 if __name__ == "__main__":
+    taskName = 'AttenSin'
+    inputSize = 3
+    dataSetSize = 12
+    assert inputSize > 0 and inputSize <= 10
+    assert dataSetSize <= 14 and dataSetSize >= 3
     try:
+        # shell script to be run as subprocess
         script = """
-set -eu
-# setup data set dir
-cur_dir="`pwd`"
-dir_name='/training-data-set/'
-training_name="$0"
-ip_size="$1"
-set_size_pow="$2"
-if [ ! -d $cur_dir$dir_name ]
-then
-    mkdir $cur_dir$dir_name
-    echo "created dir: $cur_dir$dir_name"
-fi
-if [ ! -d $cur_dir$dir_name$training_name ]
-then
-    mkdir $cur_dir$dir_name$training_name
-    echo "created dir: $cur_dir$dir_name$training_name"
-fi
-echo "input size: "$ip_size
-echo "set pow size: "$set_size_pow
+            set -eu
+            # setup data set dir
+            orig_dir="`pwd`"
+            dir_name='/training-data-set/'
+            task_name="$0"
+            ip_size="$1"
+            set_size_pow="$2"
+            set_size_pow_start=03
+            if [ ! -d $orig_dir$dir_name ]
+            then
+                mkdir $orig_dir$dir_name
+                echo "created dir: $orig_dir$dir_name"
+            fi
+            if [ ! -d $orig_dir$dir_name$task_name ]
+            then
+                mkdir $orig_dir$dir_name$task_name
+                echo "created dir: $orig_dir$dir_name$task_name"
+            fi
+            echo "input size: "$ip_size
+            echo "set pow size: "$set_size_pow
+
+            task_dir=$orig_dir$dir_name$task_name
+            ##################
+            cd $task_dir     #
+            ##################
+            existing_set="`ls`"
+            for size_pow in $(eval echo "{$set_size_pow_start..$set_size_pow}")
+            do
+                file_name=$ip_size'_'$size_pow
+                if [ "`find . -maxdepth 1 -type f -printf '%f\n' | grep $file_name`" ]
+                then 
+                    echo "data set already exists: "$file_name    
+                else
+                    touch $file_name
+                    echo "created data set file: "$file_name
+                fi
+            done
+            ##################
+            cd $orig_dir     #
+            ##################
         """
         # args to the script:
         #   $0: name for the training set (e.g.: sin: the data set should show characteristic of sin function)
@@ -34,7 +61,7 @@ echo "set pow size: "$set_size_pow
         #       e.g.:
         #           suppose user provide 10, then data set with size 2^3, 2^4, 2^5, ... 2^10 will be generated,
         #           and be stored as separate files in the corresponding folder
-        stdout, stderr = run_script(script, ['AttenSin', '3', '12'])
+        stdout, stderr = run_script(script, [taskName, str(inputSize), str(dataSetSize)])
         print("============================")
         print("script msg: \n" + str(stdout))
         print("============================")
